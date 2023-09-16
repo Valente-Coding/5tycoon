00d34e15745ef16d00ae05632feb4d865061719b
Valente-Coding <goncalo.p.valente@hotmail.com>
2023-09-16 23:51:33 +0100

    Organized And More Modular

--- resources/[SCRIPTS]/trucking/trucking.lua
+++ resources/[SCRIPTS]/trucking/trucking.lua
@@ -1,12 +1,48 @@
 local truckDepots = {
-    {coords = vector3(1208.5696, -3115.0537, 5.5403)},
-    {coords = vector3(-320.4866, -1389.6117, 36.5002)}
+    {
+        coords = vector3(1208.5696, -3115.0537, 5.5403),
+        spawnCoords = vector4(1207.7811, -3091.3716, 5.5371, 90.6691),
+        deliveryPoints = {
+            vector3(857.8163, -2346.6355, 30.3312),
+            vector3(864.7427, -2119.1694, 30.4448),
+            vector3(870.3019, -1566.9204, 30.4959),
+            vector3(158.2436, -1512.8971, 29.1397),
+            vector3(-667.3304, -1202.5023, 10.6121),
+            vector3(-596.4522, -900.6324, 25.5801),
+            vector3(-1233.1493, -253.8546, 39.0920),
+            vector3(-32.6461, -94.9570, 57.2725),
+            vector3(100.1880, 290.0016, 109.9827),
+            vector3(-990.8285, -298.5917, 37.8134),
+            vector3(-701.6105, -299.5074, 36.6379),
+            vector3(533.6143, -177.3482, 54.4632),
+            vector3(966.0208, -8.9768, 80.6905),
+        },
+    },
+    {
+        coords = vector3(-320.4866, -1389.6117, 36.5002)
+        spawnCoords = vector4(-340.8572, -1400.7527, 30.6168, 149.6355),
+        deliveryPoints = {
+            vector3(857.8163, -2346.6355, 30.3312),
+            vector3(864.7427, -2119.1694, 30.4448),
+            vector3(870.3019, -1566.9204, 30.4959),
+            vector3(158.2436, -1512.8971, 29.1397),
+            vector3(-667.3304, -1202.5023, 10.6121),
+            vector3(-596.4522, -900.6324, 25.5801),
+            vector3(-1233.1493, -253.8546, 39.0920),
+            vector3(-32.6461, -94.9570, 57.2725),
+            vector3(100.1880, 290.0016, 109.9827),
+            vector3(-990.8285, -298.5917, 37.8134),
+            vector3(-701.6105, -299.5074, 36.6379),
+            vector3(533.6143, -177.3482, 54.4632),
+            vector3(966.0208, -8.9768, 80.6905),
+        },
+    }
 }
 
 Citizen.CreateThread(function()
     local blips = {}
 
-    for _, location in pairs(truckDepos) do
+    for _, location in pairs(truckDepots) do
         local blip = AddBlipForCoord(location.coords.x, location.coords.y, location.coords.z)
         SetBlipSprite(blip, 477)
         SetBlipDisplay(blip, 4)
@@ -21,33 +57,13 @@ Citizen.CreateThread(function()
 end)
 
 
-local truckSpawnCoords1 = vector4(1207.7811, -3091.3716, 5.5371, 90.6691)
-local truckDepotCoords2 = vector4(-340.8572, -1400.7527, 30.6168, 149.6355)
 local truckMissionState = 0
-local truckDeliveryPoints = {
-    vector3(857.8163, -2346.6355, 30.3312),
-    vector3(864.7427, -2119.1694, 30.4448),
-    vector3(870.3019, -1566.9204, 30.4959),
-    vector3(158.2436, -1512.8971, 29.1397),
-    vector3(-667.3304, -1202.5023, 10.6121),
-    vector3(-596.4522, -900.6324, 25.5801),
-    vector3(-1233.1493, -253.8546, 39.0920),
-    vector3(-32.6461, -94.9570, 57.2725),
-    vector3(100.1880, 290.0016, 109.9827),
-    vector3(-990.8285, -298.5917, 37.8134),
-    vector3(-701.6105, -299.5074, 36.6379),
-    vector3(533.6143, -177.3482, 54.4632),
-    vector3(966.0208, -8.9768, 80.6905)
-}
-
-
-
 
 
 -- Function to select a random coordinate from the array
-function GetRandomDeliveryPoint()
-    local randomIndex = math.random(1, #truckDeliveryPoints)
-    return truckDeliveryPoints[randomIndex]
+function GetRandomDeliveryPoint(deliveryPoints)
+    local randomIndex = math.random(1, #deliveryPoints)
+    return deliveryPoints[randomIndex]
 end
 
 
@@ -113,20 +129,26 @@ Citizen.CreateThread(function()
     local missionVeh = nil
     local destination = nil
     local vehPed = nil
+    local currentDepot = nil
     while true do
         Citizen.Wait(1)
         local playerPed = GetPlayerPed(-1)
         local coords = GetEntityCoords(playerPed)
-        if #(coords - truckDepotCoords) < 2.0 then
-            if truckMissionState == 0 then
-                if IsControlJustReleased(0, 38) then
-                    truckMissionState = 1
-                    missionVeh = SelectVehicle()
-                    destination = GetRandomDeliveryPoint()
-                    SetNewWaypoint(destination.x, destination.y)
+
+        for _, depot in pairs (truckDepots) do 
+            if #(coords - depot.coords) < 2.0 then
+                if truckMissionState == 0 then
+                    if IsControlJustReleased(0, 38) then
+                        truckMissionState = 1
+                        missionVeh = SelectVehicle()
+                        destination = GetRandomDeliveryPoint(depot.deliveryPoints)
+                        SetNewWaypoint(destination.x, destination.y)
+                        currentDepot = depot
+                    end
                 end
             end
         end
+       
 
         vehPed = GetVehiclePedIsIn(playerPed, false)
         if missionVeh ~= nil and missionVeh == vehPed then
@@ -141,17 +163,17 @@ Citizen.CreateThread(function()
                         truckMissionState = 2
                         if truckData.level < 2 then
                             TriggerEvent('chatMessage', 'Go back to the depot to receive your paycheck.', {255, 0, 0})
-                            SetNewWaypoint(truckSpawnCoords1.x, truckSpawnCoords1.y)
+                            SetNewWaypoint(currentDepot.spawnCoords.x, currentDepot.spawnCoords.y)
                         else
-                            SetEntityCoords(missionVeh, truckSpawnCoords1.x, truckSpawnCoords1.y, truckSpawnCoords1.z, 0, 0, 0, false)
+                            SetEntityCoords(missionVeh, currentDepot.spawnCoords.x, currentDepot.spawnCoords.y, currentDepot.spawnCoords.z, 0, 0, 0, false)
                         end
                     end
                 end
             end
 
-            distance = #(vehCoords - vector3(truckSpawnCoords1.x, truckSpawnCoords1.y, truckSpawnCoords1.z))
+            distance = #(vehCoords - vector3(currentDepot.spawnCoords.x, currentDepot.spawnCoords.y, currentDepot.spawnCoords.z))
             if truckMissionState == 2 and distance < 50 then
-                DrawMarker(25, truckSpawnCoords1.x, truckSpawnCoords1.y, truckSpawnCoords1.z, 0, 0, 0, 0, 0, 0, 5.0, 5.0, 1.0, 255, 0, 0, 1.0, false, true, false, false, false, false, false)
+                DrawMarker(25, currentDepot.spawnCoords.x, currentDepot.spawnCoords.y, currentDepot.spawnCoords.z, 0, 0, 0, 0, 0, 0, 5.0, 5.0, 1.0, 255, 0, 0, 1.0, false, true, false, false, false, false, false)
                 if distance < 5 then
                     truckMissionState = 0
                     TriggerEvent('chatMessage', 'You got paid.', {255, 0, 0})
