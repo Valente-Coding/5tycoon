84a12fd6818aaf1845b72ad5db1cfb8d59552804
JustPortuguese <84799888+JustPortuguese@users.noreply.github.com>
2023-09-27 00:38:35 +0100

    tried to add options to the side menu but I'm still BAD

--- resources/[SCRIPTS]/weed-dealer/weed-dealer.lua
+++ resources/[SCRIPTS]/weed-dealer/weed-dealer.lua
@@ -14,9 +14,35 @@ function CreateMapBlip()
 
     -- Set the blip name (optional)
     BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString("Cannabis Dealer Location")
+    AddTextComponentString("Weed Dealer")
     EndTextCommandSetBlipName(blip)
 end
 
 -- Call the function to create the map blip
 CreateMapBlip()
+
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(1)
+        local playerPed = GetPlayerPed(-1)
+        local playerCoords = GetEntityCoords(playerPed)
+
+        distance = #(playerCoords - blipWeed)
+
+        if distance < 50 then
+            local menuDisplay = false
+            if distance < 3 then
+                if menuDisplay == false then
+                    menuDisplay = true
+                    TriggerEvent("side-menu:addOptions", {{id = "weed_dealer_small_package", label = "Buy loose weed", quantity = "$100"}, {id = "weed_dealer_medium_package", label = "Buy a medium brick", quantity = "$1000"}, {id = "weed_dealer_big_package", label = "Buy a big brick", quantity = "$10000"}})
+                end
+            else
+                if menuDisplay == true then
+                    menuDisplay = false
+                    TriggerEvent("side-menu:removeOptions", {{id = "weed_dealer_small_package"}, {id = "weed_dealer_medium_package"}, {id = "weed_dealer_big_package"}})
+                end
+            end
+        end
+    end
+end)
